3c5db71b1dde1a0f0cc3c1f616317a33746c4bb1
ly000082 <ly000082@algonquinlive.com>
2025-03-06 08:50:08 -0500

    added an array

--- app.js
+++ app.js
@@ -33,3 +33,35 @@ function shuffle (src) {
 /**********************************************
  * YOUR CODE BELOW
  **********************************************/
+
+const {useState, useEffect} = React;
+
+//1 An array of words must be created. The words should not include spaces or special characters. There must be at least 10 words in the array.
+const words =["vscode", "uxui", "react", "phone", "componet", "function", "return", "declaration", "variable", "coding"]
+
+//2 The game should be persistent. The player's progress should be tracked throughout the game and stored in local storage.
+
+
+//3 The player must be able to guess by typing into a textbox and hitting enter. Once a guess has been made, correct or incorrect, the textbox should be cleared. Making a guess should NOT cause a page refresh.
+
+
+//4 If the guess is correct, a new scrambled word should appear.
+
+
+
+//5 If the guess is incorrect, the current scrambled word should remain.
+
+
+//6 After each guess, the player should receive a response telling whether the guess was correct or incorrect.
+
+
+//7 After each guess, the number of points or strikes should be updated and displayed for the player.
+
+
+//8 If the player clicks on the pass button and has at least one pass remaining, the current word should be removed, and the next scrambled word should be displayed.
+
+
+//9 If the player makes it through the entire list of words OR receives the maximum number of strikes, the game should end and give the player the option to play again.
+
+
+//10 If the player chooses to play again, the game should restart, clearing all previous points, strikes, or used passes. The restart should be accomplished without a page refresh.
